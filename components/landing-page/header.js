da05683380b6b1cc0bcd041fb8a674fb660088f4
data-doge <eugene@enspiral.com>
2016-08-31 14:29:56 -0400

    fixed header

---
+++ components/landing-page/header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+
+class Header extends Component {
+  render () {
+    return (
+      <div className="landing-page__header">
+        <div className="landing-page__header">
+          <AppBar
+            showMenuIconButton={false}
+            title={<span>GC</span>}
+            iconElementRight={
+              <IconMenu
+                iconButtonElement={
+                  <IconButton><MoreVertIcon /></IconButton>
+                }
+                targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+              >
+                <MenuItem primaryText="Intro" />
+                <MenuItem primaryText="How It Works" />
+                <MenuItem primaryText="Our Mission" />
+                <MenuItem primaryText="Contact Us" />
+              </IconMenu>
+            }
+          >
+          </AppBar>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
--- components/landing-page/index.js
+++ components/landing-page/index.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react'
-import RaisedButton from 'material-ui/RaisedButton'
+import Header from './header.js'
 
 class LandingPage extends Component {
   render () {
     return (
       <div className="landing-page">
-        <div className="landing-page__header">
-          <RaisedButton label="O SHIT" />
-        </div>
-
-        <div className="landing-page__intro-panel"></div>
-
-        <div className="landing-page__mission-panel"></div>
-
-        <div className="landing-page__contact-panel"></div>
-
-        <div className="landing-page__footer"></div>
+        <Header />
       </div>
     )
   }
