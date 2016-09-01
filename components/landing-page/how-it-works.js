d8762b0008c8624a6fd0227c265e672811850b15
data-doge <eugene@enspiral.com>
2016-09-01 18:05:35 -0400

    intro panel

--- components/landing-page/how-it-works.js
+++ components/landing-page/how-it-works.js
@@ -5,13 +5,6 @@ class HowItWorks extends Component {
   render () {
     return (
       <div className="landing-page__how-it-works" name="landing-page__how-it-works">
-        <h2 className="landing-page__subheader">How It Works</h2>
-        <p className="landing-page__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <RaisedButton
-          className="landing-page__learn-more-btn"
-          label="ima button"
-          primary={true}
-        />
       </div>
     )
   }
--- components/landing-page/intro.js
+++ components/landing-page/intro.js
@@ -5,13 +5,25 @@ class Intro extends Component {
   render () {
     return (
       <div className="landing-page__intro" name="landing-page__intro">
-        <h2 className="landing-page__subheader">Intro Panel</h2>
-        <p className="landing-page__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <img className="landing-page__phone-img" src="./assets/imgs/phone_img.svg" />
+        <img className="landing-page__handcuffs-img" src="./assets/imgs/handcuffs_img.svg" />
+
+        <h2 className="landing-page__intro-header">[product definition of GC]</h2>
+
+        <h3 className="landing-page__intro-subheader">
+          No one expects to be arrested, but if you’re at the wrong place, at the wrong time, just call 1-800-GOOD CALL
+        </h3>
+
         <RaisedButton
-          className="landing-page__learn-more-btn"
-          label="Learn More"
-          primary={true}
+          className="landing-page__green-btn"
+          label="sign up"
+          backgroundColor="#40B097"
+          labelColor="#FFFFFF"
         />
+
+        <div className="landing-page__intro-see-how-it-works-txt">
+          See how it works
+        </div>
       </div>
     )
   }
