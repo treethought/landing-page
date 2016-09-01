ab76d6a8279b843253984c698eb078f70d7bc3e4
data-doge <eugene@enspiral.com>
2016-09-01 16:41:45 -0400

    header

--- components/landing-page/header.js
+++ components/landing-page/header.js
@@ -4,25 +4,27 @@ import IconButton from 'material-ui/IconButton'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import SvgIcon from 'material-ui/SvgIcon'
+import FlatButton from 'material-ui/FlatButton'
 
 class Header extends Component {
   render () {
     return (
       <div className="landing-page__header">
         <AppBar
+          className="landing-page__app-bar"
           showMenuIconButton={false}
-          title={<span>GC</span>}
+          zDepth={0}
+          title={
+            <img src="./assets/imgs/logo.svg"/>
+          }
           iconElementRight={
-            <IconMenu
-              iconButtonElement={
-                <IconButton><MoreVertIcon /></IconButton>
-              }
-              targetOrigin={{horizontal: 'right', vertical: 'top'}}
-              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-            >
-              <MenuItem primaryText="Intro" onTouchTap={this.props.scrollToIntro}/>
-              <MenuItem primaryText="How It Works" onTouchTap={this.props.scrollToHowItWorks}/>
-            </IconMenu>
+            <nav>
+              <FlatButton className="landing-page__header-nav-btn" label="How it works" />
+              <FlatButton className="landing-page__header-nav-btn" label="About us" />
+              <FlatButton className="landing-page__header-nav-btn" label="Contact us" />
+              <FlatButton className="landing-page__header-nav-btn" label="Log in" />
+            </nav>
           }
         >
         </AppBar>
