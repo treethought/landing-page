1a181ed088a99ddf89fc61c82c920bc0125eef38
data-doge <eugene@enspiral.com>
2016-09-02 16:03:40 -0400

    remove smoothscroll shit for now

--- components/landing-page/index.js
+++ components/landing-page/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import {findDOMNode} from 'react-dom'
-import $ from 'jquery'
 import Header from './header.js'
 import Intro from './intro.js'
 import HowItWorks from './how-it-works.js'
@@ -9,26 +7,14 @@ import ContactUs from './contact-us.js'
 import Footer from './footer.js'
 
 class LandingPage extends Component {
-  scrollTo (ref) {
-    return () => {
-      let $targetEl = $(findDOMNode(this.refs[ref])), $thisEl = $(findDOMNode(this))
-      $('html, body').animate({
-        scrollTop: $targetEl.offset().top - parseInt($thisEl.css('margin-top'))
-      }, 500)
-    }
-  }
-
   render () {
     return (
       <div className="landing-page">
-        <Header
-          scrollToIntro={this.scrollTo('intro').bind(this)}
-          scrollToHowItWorks={this.scrollTo('how-it-works').bind(this)}
-        />
-        <Intro ref="intro" />
-        <HowItWorks ref="how-it-works" />
-        <AboutUs ref="about-us" />
-        <ContactUs ref="contact-us" />
+        <Header />
+        <Intro />
+        <HowItWorks />
+        <AboutUs />
+        <ContactUs />
         <Footer />
       </div>
     )
