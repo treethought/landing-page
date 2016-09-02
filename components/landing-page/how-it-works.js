6ca0b416d24e63adf9671a6eaeef4e7edb60f40d
data-doge <eugene@enspiral.com>
2016-09-01 22:37:30 -0400

    how it works panel

--- components/landing-page/how-it-works.js
+++ components/landing-page/how-it-works.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
 import RaisedButton from 'material-ui/RaisedButton'
+import { Grid, Row, Cell } from 'react-inline-grid'
 
 class HowItWorks extends Component {
   render () {
     return (
       <div className="landing-page__how-it-works" name="landing-page__how-it-works">
+        <h2 className="landing-page__how-it-works-header">Make your call count.</h2>
+        <h3 className="landing-page__how-it-works-subheader">How it works</h3>
+
+        <Grid>
+          <Row is="around">
+            <Cell is="3">
+              <img className="landing-page__how-it-works-icon" src="./assets/imgs/signup_icon.svg"></img>
+              <p className="landing-page__how-it-works-text">
+                <div className="landing-page__how-it-works-step-number">1</div>
+                Provide us with an emergency contact
+              </p>
+            </Cell>
+
+            <Cell is="3">
+              <img className="landing-page__how-it-works-icon" src="./assets/imgs/phone_icon.svg"></img>
+              <p className="landing-page__how-it-works-text">
+                <div className="landing-page__how-it-works-step-number">2</div>
+                If you're arrested, call 1-800 GOOD CALL. Leave a message for your loved ones if you can't reach them.
+              </p>
+            </Cell>
+
+            <Cell is="3">
+              <img className="landing-page__how-it-works-icon" src="./assets/imgs/balance_icon.svg"></img>
+              <p className="landing-page__how-it-works-text">
+                <div className="landing-page__how-it-works-step-number">3</div>
+                If you’re arrested, call 1-800 GOOD CALL. Leave a message for your loved ones.
+              </p>
+            </Cell>
+          </Row>
+        </Grid>
+
+        <RaisedButton
+          className="landing-page__green-btn landing-page__sample-call-btn"
+          label="try a sample call"
+          backgroundColor="#40B097"
+          labelColor="#FFFFFF"
+        />
       </div>
     )
   }
--- components/landing-page/intro.js
+++ components/landing-page/intro.js
@@ -8,11 +8,11 @@ class Intro extends Component {
         <img className="landing-page__phone-img" src="./assets/imgs/phone_img.svg" />
         <img className="landing-page__handcuffs-img" src="./assets/imgs/handcuffs_img.svg" />
 
-        <h2 className="landing-page__intro-header">[product definition of GC]</h2>
+        <h1 className="landing-page__intro-header">[product definition of GC]</h1>
 
-        <h3 className="landing-page__intro-subheader">
+        <h2 className="landing-page__intro-subheader">
           No one expects to be arrested, but if you’re at the wrong place, at the wrong time, just call 1-800-GOOD CALL
-        </h3>
+        </h2>
 
         <RaisedButton
           className="landing-page__green-btn"
