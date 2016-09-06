2e4676a736c50cfdd495f4df4b0e658effe4ddd0
data-doge <eugene@enspiral.com>
2016-09-06 15:24:23 -0400

    responsive footer

--- components/landing-page/footer.js
+++ components/landing-page/footer.js
@@ -8,17 +8,17 @@ class Footer extends Component {
         <div className="landing-page__footer-link-container">
           <Grid>
             <Row is="start">
-              <Cell is="5">
+              <Cell is="5 tablet-4">
                 <div className="landing-page__footer-header">Contact Us</div>
-                <div className="landing-page__footer-link">150 Court St. 2nd Floor, Brooklyn 11201</div>
-                <div className="landing-page__footer-link">hello@goodcall.com</div>
+                <a className="landing-page__footer-link">150 Court St. 2nd Floor, Brooklyn 11201</a>
+                <a className="landing-page__footer-link">hello@goodcall.com</a>
               </Cell>
 
-              <Cell is="5">
+              <Cell is="5 tablet-4">
                 <div className="landing-page__footer-header">Help</div>
-                <div className="landing-page__footer-link">Privacy Policy</div>
-                <div className="landing-page__footer-link">Terms & Conditions</div>
-                <div className="landing-page__footer-link">Your Rights</div>
+                <a className="landing-page__footer-link">Privacy Policy</a>
+                <a className="landing-page__footer-link">Terms & Conditions</a>
+                <a className="landing-page__footer-link">Your Rights</a>
               </Cell>
             </Row>
           </Grid>
