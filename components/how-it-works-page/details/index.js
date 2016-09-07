c5efebc0451ec3648ec45a5a63d40e0e31576b15
data-doge <eugene@enspiral.com>
2016-09-07 15:10:13 -0400

    some progress on making how-it-works-page responsive

--- components/how-it-works-page/details/index.js
+++ components/how-it-works-page/details/index.js
@@ -34,14 +34,14 @@ class Details extends Component {
               </li>
 
               {renderIf(i < arr.length - 1)(
-                <div className="how-it-works-page__vert-white-line how-it-works-page__vert-line"></div>
+                <div className="how-it-works-page__line how-it-works-page__vert-white-line how-it-works-page__vert-line"></div>
               )}
             </div>
           ))}
 
           {callSubsteps.map((substep, i) => (
             <div key={i}>
-              <div className="how-it-works-page__short-vert-green-line how-it-works-page__vert-line"></div>
+              <div className="how-it-works-page__line how-it-works-page__short-vert-green-line how-it-works-page__vert-line"></div>
 
               <li className="how-it-works-page__details-centered-row">
                 <div className="how-it-works-page__sub-step-number-icon-container">
@@ -49,15 +49,16 @@ class Details extends Component {
                     <div className="how-it-works-page__sub-step-number">{substep.number}</div>
                   </div>
                 </div>
-
+                <span className="how-it-works-page__step-horiz-separator"></span>
+                
                 <span className="how-it-works-page__step-text">{substep.text}</span>
               </li>
             </div>
           ))}
 
-          <div className="how-it-works-page__vert-green-line how-it-works-page__vert-line"></div>
+          <div className="how-it-works-page__line how-it-works-page__vert-green-line how-it-works-page__vert-line"></div>
 
-          <div className="how-it-works-page__green-branched-line how-it-works-page__branched-line"></div>
+          <div className="how-it-works-page__line how-it-works-page__green-branched-line how-it-works-page__branched-line"></div>
 
           <li className="how-it-works-page__details-centered-row">
             <div className="how-it-works-page__parallel-step-icon-container how-it-works-page__details-centered-row">
@@ -68,9 +69,9 @@ class Details extends Component {
             <span className="how-it-works-page__step-text">Good Call will alert a lawyer for you and get your message to your loved ones</span>
           </li>
 
-          <div className="how-it-works-page__white-branched-line how-it-works-page__branched-line"></div>
+          <div className="how-it-works-page__line how-it-works-page__white-branched-line how-it-works-page__branched-line"></div>
 
-          <div className="how-it-works-page__short-vert-white-line how-it-works-page__vert-line"></div>
+          <div className="how-it-works-page__line how-it-works-page__short-vert-white-line how-it-works-page__vert-line"></div>
 
           <li className="how-it-works-page__details-centered-row">
             <img className="how-it-works-page__step-icon" src="./assets/imgs/free_icon.svg" />
