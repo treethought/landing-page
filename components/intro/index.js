b465680a8844343172d412f2eff7a67600754ed2
data-doge <eugene@enspiral.com>
2016-09-08 18:19:07 -0400

    use render-if on about-page

--- components/intro/index.js
+++ components/intro/index.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
+import renderIf from 'render-if'
 
 class Intro extends Component {
   render () {
-    let subHeading;
-    if(this.props.subheading) {
-      subHeading = <h2 className="intro__subheader">{this.props.subheading}</h2>;
-    }
-
     return (
       <div className="intro">
         <h1 className="intro__header">{this.props.heading}</h1>
 
-        {subHeading}
+        {renderIf(this.props.subheading) (
+          <h2 className="intro__subheader">{this.props.subheading}</h2>
+        )}
 
         {this.props.children}
       </div>
