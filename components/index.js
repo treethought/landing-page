1d133b08b8051a22611f4103a2fa40f03f85919a
data-doge <eugene@enspiral.com>
2016-09-08 10:46:59 -0400

    scroll to top on route change

--- components/index.js
+++ components/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Router, Route, browserHistory} from 'react-router'
+import {Router, Route, browserHistory, applyRouterMiddleware} from 'react-router'
+import {useScroll} from 'react-router-scroll'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import LandingPage from './landing-page'
 import HowItWorksPage from './how-it-works-page'
 
 export default () => (
   <MuiThemeProvider>
-    <Router history={browserHistory}>
+    <Router history={browserHistory} render={applyRouterMiddleware(useScroll())}>
       <Route path="/" component={LandingPage}/>
       <Route path="/how-it-works" component={HowItWorksPage}/>
     </Router>
