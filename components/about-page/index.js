154c7c857427faa6b7117d195a232e7f91e35018
Ethan Resnick <ethan.resnick@gmail.com>
2016-09-08 12:52:33 -0400

    Stub out about page structure

--- components/about-page/index.js
+++ components/about-page/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import InnerPage from '../inner-page'
+import Intro from './../intro'
+import Mission from './../landing-page/mission'
 
 class AboutPage extends Component {
   render () {
     return (
       <InnerPage>
-        <p>Hello!</p>
+        <Intro heading="About us" subheading="Some text here">
+          <p>More, more more</p>
+        </Intro>
+        <Mission />
       </InnerPage>
     )
   }
