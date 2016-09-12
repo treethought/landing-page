4629f44be8144d3bbd3dd7cbf6d28567eff994a5
data-doge <eugene@enspiral.com>
2016-09-12 12:46:56 -0400

    remove how-it-works intro for now

--- components/how-it-works-page/index.js
+++ components/how-it-works-page/index.js
@@ -13,16 +13,6 @@ class HowItWorksPage extends Component {
     return (
       <div className="how-it-works-page">
         <Header />
-        <Intro heading="we got your back." subheading="Call us if you’re arrested and we will alert a lawyer and your loved ones for you.">
-          <RaisedButton
-            className="gc-std-btn how-it-works-page__intro-sign-up-btn"
-            label="sign up for updates"
-            backgroundColor="#40B097"
-            labelColor="#FFFFFF"
-          />
-
-          <ScrollDownBtn to="how-it-works-page__details" text="see what the call looks like"/>
-        </Intro>
         <Element name="how-it-works-page__details">
           <Details />
         </Element>
