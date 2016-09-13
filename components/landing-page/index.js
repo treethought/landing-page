a1e2acb5a1211a72999018fc4bd451c0692545e3
data-doge <eugene@enspiral.com>
2016-09-12 22:11:43 -0400

    move landing-page into inner-page

--- components/landing-page/index.js
+++ components/landing-page/index.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
-import Header from './../header'
+import React, {Component} from 'react'
+import InnerPage from './../inner-page'
 import Stories from './stories'
 import Problem from './problem'
 import HowItWorks from './how-it-works'
 import LaunchingSoon from './launching-soon'
-import Footer from './../footer'
 import Scroll from 'react-scroll'
 const {Element} = Scroll
 
 class LandingPage extends Component {
   render () {
     return (
-      <div className="landing-page">
-        <Header />
-        <Stories />
-        <Element name="landing-page__problem">
-          <Problem />
-        </Element>
-        <section className="landing-page__blurb-container">
-          <p className="landing-page__blurb">
-            We are helping people who are arrested to reach their loved ones and to alert a lawyer earlier on in the arrest process for a more fair court outcome.
-          </p>
-        </section>
-        <HowItWorks />
-        <LaunchingSoon />
-        <Footer />
-      </div>
+      <InnerPage>
+        <div className="landing-page">
+          <Stories />
+          <Element name="landing-page__problem">
+            <Problem />
+          </Element>
+          <section className="landing-page__blurb-container">
+            <p className="landing-page__blurb">
+              We are helping people who are arrested to reach their loved ones and to alert a lawyer earlier on in the arrest process for a more fair court outcome.
+            </p>
+          </section>
+          <HowItWorks />
+          <LaunchingSoon />
+        </div>
+      </InnerPage>
     )
   }
 }
