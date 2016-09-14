753b20136012d68b8648e4f65c02752dd8a4297b
data-doge <eugene@enspiral.com>
2016-09-14 17:40:27 -0400

    move components into app/

--- app/index.js
+++ app/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {Router, Route, browserHistory, applyRouterMiddleware} from 'react-router'
 import {useScroll} from 'react-router-scroll'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import LandingPage from './landing-page'
-// import AboutPage from "./about-page" // leaving out for now
-import ErrorPage from './error-page'
+import LandingPage from './components/landing-page'
+// import AboutPage from "./components/about-page" // leaving out for now
+import ErrorPage from './components/error-page'
 
 export default () => (
   <MuiThemeProvider>
