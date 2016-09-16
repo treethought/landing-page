5f7cd1f59c292326301d4bc2436e44ac360931c3
data-doge <eugene@enspiral.com>
2016-09-16 15:07:07 -0400

    update staging scripts

--- app/config/index.js
+++ app/config/index.js
@@ -1,14 +1,12 @@
 const env = process.env.NODE_ENV || 'development'
+const isStaging = process.env.IS_STAGING
 
 const environmentConfigs = {
   development: {
     apiBaseUrl: 'http://localhost:3000'
   },
-  staging: {
-    apiBaseUrl: 'https://good-call-nyc-staging-api.herokuapp.com'
-  },
   production: {
-    apiBaseUrl: 'https://good-call-nyc-api.herokuapp.com'
+    apiBaseUrl: isStaging ? 'https://good-call-nyc-staging-api.herokuapp.com' : 'https://good-call-nyc-api.herokuapp.com'
   }
 }
 
