6dbe80e8da793bd66d9002ad462541e6fc5bcf01
data-doge <eugene@enspiral.com>
2016-09-16 21:01:41 -0400

    get dialog hint working on createUser page

--- app/components/sign-up-page/create-user-form/index.js
+++ app/components/sign-up-page/create-user-form/index.js
@@ -5,6 +5,8 @@ import MenuItem from 'material-ui/MenuItem'
 import FlatButton from 'material-ui/FlatButton'
 import isEmpty from 'lodash.isempty'
 import {findDOMNode} from 'react-dom'
+import MediaQuery from 'react-responsive'
+import Dialog from 'material-ui/Dialog'
 
 const userFields = [
   {name: 'name', label: 'First Name, Last Name'},
@@ -25,10 +27,26 @@ const heardAboutUsThroughOpts = [
 ]
 
 class CreateUserForm extends Component {
+  constructor () {
+    super()
+    this.state = {hintShown: false, hintClosed: false}
+  }
+
   showHint () {
-    let {hint, formFields} = this.refs
-    hint.style.display = 'flex'
-    formFields.style.textAlign = 'right'
+    if (window.innerWidth > 640) {
+      let {hint, formFields} = this.refs
+      hint.style.display = 'flex'
+      formFields.style.textAlign = 'right'
+    } else {
+      if (!this.state.hintShown) {
+        this.setState({hintShown: true})
+      }
+    }
+  }
+
+  closeHintDialog () {
+    console.log('meow')
+    this.setState({hintClosed: true})
   }
 
   render () {
@@ -37,11 +55,29 @@ class CreateUserForm extends Component {
         <div className="sign-up-page__form-hint-bubble-container" ref="hint">
           <div className="sign-up-page__form-hint-bubble">
             <p className="sign-up-page__form-hint-text">
-              Your information will only be used by Good Call to verify you in case of an arrest and by your lawyer for your case.  
+              Your information will only be used by Good Call to verify you in case of an arrest and by your lawyer for your case.
             </p>
           </div>
           <div className="sign-up-page__form-hint-bubble-arrow"></div>
         </div>
+
+        <Dialog
+          actions={
+            <FlatButton
+              label="GOT IT"
+              labelStyle={{color: "#FDFFF9", fontSize: "14px", letterSpacing: "0.5px"}}
+              onTouchTap={this.closeHintDialog.bind(this)}
+            />
+          }
+          contentStyle={{fontSize: "16px", color: "#FDFFF9", lineHeight: "24px", fontWeight: "300"}}
+          bodyStyle={{background: "#40B097", color: "#FDFFF9"}}
+          actionsContainerStyle={{background: "#40B097"}}
+          modal={false}
+          open={this.state.hintShown && !this.state.hintClosed}
+          onRequestClose={this.closeHintDialog.bind(this)}
+        >
+          Your information will only be used by Good Call to verify you in case of an arrest and by your lawyer for your case.
+        </Dialog>
 
         <div className="sign-up-page__form-fields-container" ref="formFields">
           {userFields.map((field, i) => (
@@ -64,23 +100,45 @@ class CreateUserForm extends Component {
             />
           ))}
 
-          <SelectField
-            autoWidth={true}
-            className="sign-up-page__form-select-field"
-            fullWidth={true}
-            iconStyle={{fill: '#40B097', transform: 'rotateZ(180deg)', right: '5px', top: '12px'}}
-            hintText="How did you learn about Good Call?"
-            hintStyle={{fontSize: '16px', color: '#4A4A4A', fontWeight: '300', textAlign: 'left'}}
-            menuStyle={{color: '#000000'}}
-            onChange={this.props.setUser('heardAboutUsThrough')}
-            style={{boxShadow: '0px 2px 4px 0px rgba(0,0,0,0.24)', paddingLeft: '11px', paddingTop: '3px', marginTop: '6px', textAlign: 'left'}}
-            underlineStyle={{borderColor: 'transparent'}}
-            value={this.props.user.heardAboutUsThrough}
-          >
-            {heardAboutUsThroughOpts.map((opt, i) => (
-              <MenuItem key={i} value={opt} primaryText={opt} style={{background: '#FFFFFF'}} />
-            ))}
-          </SelectField>
+          <MediaQuery query="(min-width: 640px)">
+            <SelectField
+              autoWidth={true}
+              className="sign-up-page__form-select-field"
+              fullWidth={true}
+              iconStyle={{fill: '#40B097', right: '5px', top: '12px'}}
+              hintText="How did you hear about Good Call?"
+              hintStyle={{fontSize: '16px', color: '#4A4A4A', fontWeight: '300', textAlign: 'left'}}
+              menuStyle={{color: '#000000'}}
+              onChange={this.props.setUser('heardAboutUsThrough')}
+              style={{boxShadow: '0px 2px 4px 0px rgba(0,0,0,0.24)', paddingLeft: '11px', paddingTop: '3px', marginTop: '6px', textAlign: 'left'}}
+              underlineStyle={{borderColor: 'transparent'}}
+              value={this.props.user.heardAboutUsThrough}
+            >
+              {heardAboutUsThroughOpts.map((opt, i) => (
+                <MenuItem key={i} value={opt} primaryText={opt} style={{background: '#FFFFFF'}} />
+              ))}
+            </SelectField>
+          </MediaQuery>
+
+          <MediaQuery query="(max-width: 639px)">
+            <SelectField
+              autoWidth={true}
+              className="sign-up-page__form-select-field"
+              fullWidth={true}
+              iconStyle={{fill: '#40B097', right: '5px', top: '12px'}}
+              hintText="How did you hear about Good Call?"
+              hintStyle={{fontSize: '14px', color: '#4A4A4A', fontWeight: '300', textAlign: 'left'}}
+              menuStyle={{color: '#000000'}}
+              onChange={this.props.setUser('heardAboutUsThrough')}
+              style={{boxShadow: '0px 2px 4px 0px rgba(0,0,0,0.24)', paddingLeft: '11px', paddingTop: '3px', marginTop: '6px', textAlign: 'left'}}
+              underlineStyle={{borderColor: 'transparent'}}
+              value={this.props.user.heardAboutUsThrough}
+            >
+              {heardAboutUsThroughOpts.map((opt, i) => (
+                <MenuItem key={i} value={opt} primaryText={opt} style={{background: '#FFFFFF'}} />
+              ))}
+            </SelectField>
+          </MediaQuery>
 
           <FlatButton
             className="gc-std-btn sign-up-page__form-continue-btn"
