36602ff6648696c603df594956d852f5db9d9d50
data-doge <eugene@enspiral.com>
2016-09-16 22:26:42 -0400

    link to twitter

--- app/components/sign-up-success-page/index.js
+++ app/components/sign-up-success-page/index.js
@@ -19,8 +19,8 @@ class SignUpSuccessPage extends Component {
 
             <div className="sign-up-success-page__follow-us-panel sign-up-success-page__panel">
               <h3 className="sign-up-success-page__panel-header">Follow us and stay updated.</h3>
-              <a className="sign-up-success-page__link">@goodcallnyc</a>
-              <a className="sign-up-success-page__link">#goodcallnyc</a>
+              <a className="sign-up-success-page__link" href="https://twitter.com/GoodCallNYC">@goodcallnyc</a>
+              {/*<a className="sign-up-success-page__link">#goodcallnyc</a>*/}
             </div>
 
             <div className="sign-up-success-page__learn-about-us-panel sign-up-success-page__panel">
