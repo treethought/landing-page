724e5d15c6712c54e91e1faeae00e4920ab70677
data-doge <eugene@enspiral.com>
2016-10-06 14:41:48 -0400

    add facebook icon

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -6,53 +6,56 @@ const helpLinks = [
   {to: '/sign-up', label: 'Sign Up'},
   {to: '/faq', label: 'FAQ'},
   {to: '/about-us', label: 'About Us'},
-  {to: '/privacy-policy', label: 'Privacy Policy'},
+  {to: '/privacy-policy', label: 'Privacy Policy'}
 ]
 
 class Footer extends Component {
   render () {
     return (
-      <footer className="landing-page__footer">
+      <footer className='landing-page__footer'>
         <Grid>
-          <Row is="start">
-            <Cell is="5 tablet-4">
-              <div className="landing-page__footer-header">Contact Us</div>
+          <Row is='start'>
+            <Cell is='5 tablet-4'>
+              <div className='landing-page__footer-header'>Contact Us</div>
 
-              <div className="landing-page__footer-links-container">
-                <address className="landing-page__footer-address-link-container">
-                  <a href="https://www.google.com/maps?q=150+Court+St.+2nd+Floor,+Brooklyn+11201" className="landing-page__footer-link">150 Court St. 2nd Floor, Brooklyn 11201</a>
+              <div className='landing-page__footer-links-container'>
+                <address className='landing-page__footer-address-link-container'>
+                  <a href='https://www.google.com/maps?q=150+Court+St.+2nd+Floor,+Brooklyn+11201' className='landing-page__footer-link'>150 Court St. 2nd Floor, Brooklyn 11201</a>
                 </address>
 
-                <a href="mailto:hello@goodcall.nyc" className="landing-page__footer-link">hello@goodcall.nyc</a>
+                <a href='mailto:hello@goodcall.nyc' className='landing-page__footer-link'>hello@goodcall.nyc</a>
 
-                <div className="landing-page__footer-social-media-links-container">
-                  <a href="https://twitter.com/GoodCallNYC" className="landing-page__footer-social-media-link">
-                    <img src="/assets/imgs/twitter_icon.svg" className="landing-page__footer-social-media-link-icon" />
+                <div className='landing-page__footer-social-media-links-container'>
+                  <a href='https://www.facebook.com/goodcallnyc' className='landing-page__footer-social-media-link'>
+                    <img src='/assets/imgs/facebook_icon.svg' className='landing-page__footer-social-media-link-icon' />
                   </a>
-                  <a href="https://www.instagram.com/goodcallnyc/" className="landing-page__footer-social-media-link">
-                    <img src="/assets/imgs/instagram_icon.svg" className="landing-page__footer-social-media-link-icon" />
+                  <a href='https://twitter.com/GoodCallNYC' className='landing-page__footer-social-media-link'>
+                    <img src='/assets/imgs/twitter_icon.svg' className='landing-page__footer-social-media-link-icon' />
+                  </a>
+                  <a href='https://www.instagram.com/goodcallnyc/' className='landing-page__footer-social-media-link'>
+                    <img src='/assets/imgs/instagram_icon.svg' className='landing-page__footer-social-media-link-icon' />
                   </a>
                 </div>
               </div>
             </Cell>
 
-            <Cell is="3 tablet-4">
-              <div className="landing-page__footer-header">Help</div>
+            <Cell is='3 tablet-4'>
+              <div className='landing-page__footer-header'>Help</div>
 
-              <div className="landing-page__footer-links-container">
+              <div className='landing-page__footer-links-container'>
                 {helpLinks.map((link, i) => (
-                  <Link key={i} to={link.to} className="landing-page__footer-link">{link.label}</Link>
+                  <Link key={i} to={link.to} className='landing-page__footer-link'>{link.label}</Link>
                 ))}
               </div>
             </Cell>
 
-            <Cell is="4 tablet-4">
-              <div className="landing-page__footer-header">Our Partners</div>
+            <Cell is='4 tablet-4'>
+              <div className='landing-page__footer-header'>Our Partners</div>
 
-              <div className="landing-page__footer-links-container">
+              <div className='landing-page__footer-links-container'>
                 <div>
-                  <a className="landing-page__footer-link" href="https://labs.robinhood.org/">
-                    <img src="/assets/imgs/blueridgelabs.png"></img>
+                  <a className='landing-page__footer-link' href='https://labs.robinhood.org/'>
+                    <img src='/assets/imgs/blueridgelabs.png'></img>
                   </a>
                 </div>
               </div>
--- app/components/sign-up-success-page/index.js
+++ app/components/sign-up-success-page/index.js
@@ -51,36 +51,40 @@ class SignUpSuccessPage extends Component {
   render () {
     return (
       <InnerPage location={this.props.location}>
-        <div className="sign-up-success-page">
-          <h1 className="sign-up-success-page__header">Thank you for joining the movement</h1>
-          <h2 className="sign-up-success-page__subheader">we will keep you updated about when our pilot launches</h2>
+        <div className='sign-up-success-page'>
+          <h1 className='sign-up-success-page__header'>Thank you for joining the movement</h1>
+          <h2 className='sign-up-success-page__subheader'>we will keep you updated about when our pilot launches</h2>
 
-          <div className="sign-up-success-page__panels">
-            <div className="sign-up-success-page__get-in-touch-panel sign-up-success-page__panel">
-              <h3 className="sign-up-success-page__panel-header">Get in touch with us.</h3>
-              <a className="sign-up-success-page__link" href="mailto:hello@goodcall.nyc">hello@goodcall.nyc</a>
+          <div className='sign-up-success-page__panels'>
+            <div className='sign-up-success-page__get-in-touch-panel sign-up-success-page__panel'>
+              <h3 className='sign-up-success-page__panel-header'>Get in touch with us.</h3>
+              <a className='sign-up-success-page__link' href='mailto:hello@goodcall.nyc'>hello@goodcall.nyc</a>
             </div>
 
-            <div className="sign-up-success-page__follow-us-panel sign-up-success-page__panel">
-              <h3 className="sign-up-success-page__panel-header">Follow us and stay updated.</h3>
-              <div className="sign-up-success-page__links">
-                  <a className="sign-up-success-page__link" href="https://twitter.com/GoodCallNYC">
-                  <img className="sign-up-success-page__link-icon" src="/assets/imgs/twitter_icon.svg" />
+            <div className='sign-up-success-page__follow-us-panel sign-up-success-page__panel'>
+              <h3 className='sign-up-success-page__panel-header'>Follow us and stay updated.</h3>
+              <div className='sign-up-success-page__links'>
+                <a className='sign-up-success-page__link' href='https://www.facebook.com/goodcallnyc'>
+                  <img className='sign-up-success-page__link-icon' src='/assets/imgs/facebook_icon.svg' />
                 </a>
 
-                <a className="sign-up-success-page__link" href="https://www.instagram.com/goodcallnyc">
-                  <img className="sign-up-success-page__link-icon" src="/assets/imgs/instagram_icon.svg" />
+                <a className='sign-up-success-page__link' href='https://twitter.com/GoodCallNYC'>
+                  <img className='sign-up-success-page__link-icon' src='/assets/imgs/twitter_icon.svg' />
+                </a>
+
+                <a className='sign-up-success-page__link' href='https://www.instagram.com/goodcallnyc'>
+                  <img className='sign-up-success-page__link-icon' src='/assets/imgs/instagram_icon.svg' />
                 </a>
               </div>
             </div>
 
-            <div className="sign-up-success-page__learn-about-us-panel sign-up-success-page__panel">
-              <h3 className="sign-up-success-page__panel-header">Learn about what we're doing</h3>
+            <div className='sign-up-success-page__learn-about-us-panel sign-up-success-page__panel'>
+              <h3 className='sign-up-success-page__panel-header'>Learn about what we're doing</h3>
               <FlatButton
-                className="gc-std-btn sign-up-success-page__mission-btn"
-                containerElement={<Link to="/about-us"/>}
-                label="read our mission"
-                backgroundColor="#FDFFF9"
+                className='gc-std-btn sign-up-success-page__mission-btn'
+                containerElement={<Link to='/about-us'/>}
+                label='read our mission'
+                backgroundColor='#FDFFF9'
               />
             </div>
           </div>
