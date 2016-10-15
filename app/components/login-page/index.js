edca901644b2638d9db9a88c244adfbf31c02c7a
data-doge <eugene@enspiral.com>
2016-10-15 02:26:01 -0400

    client-side email/phone validation

--- app/components/login-page/index.js
+++ app/components/login-page/index.js
@@ -4,8 +4,17 @@ import StandardTextField from './../standard-text-field'
 import FlatButton from 'material-ui/FlatButton'
 
 class LoginPage extends Component {
+  emailOrPhoneIsValid () {
+    const { emailOrPhone } = this.props
+    const isValidEmail = /(.+)@(.+){2,}\.(.+){2,}/.test(emailOrPhone)
+    let trimmedPhoneDigits = emailOrPhone.trim().replace(/\D/g, '')
+    let phoneDigits = trimmedPhoneDigits[0] === '1' ? trimmedPhoneDigits.substring(1) : trimmedPhoneDigits
+    const isValidPhone = phoneDigits.length === 10
+    return isValidEmail || isValidPhone
+  }
+
   render () {
-    const { redirectError } = this.props
+    const { redirectError, setEmailOrPhone, requestInProgress, getAccessCode } = this.props
 
     return (
       <div className='login-page'>
@@ -20,12 +29,15 @@ class LoginPage extends Component {
         <div className='login-page__text-field-container'>
           <StandardTextField
             labelText='Email or Cell number (xxx) xxx-xxxx'
+            onChange={setEmailOrPhone}
           />
         </div>
 
         <FlatButton
           className='gc-std-btn login-page__login-code-btn'
           label='send me a login code'
+          onClick={getAccessCode}
+          disabled={requestInProgress || !this.emailOrPhoneIsValid()}
         />
 
         <a className='login-page__lil-link'>Already have an access code?</a>
@@ -34,9 +46,13 @@ class LoginPage extends Component {
   }
 }
 
-const { string } = PropTypes
+const { string, func, bool } = PropTypes
 LoginPage.propTypes = {
-  redirectError: string
+  redirectError: string,
+  setEmailOrPhone: func,
+  emailOrPhone: string,
+  requestInProgress: bool,
+  getAccessCode: func
 }
 
 export default LoginPage
--- app/containers/login-page-container.js
+++ app/containers/login-page-container.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import LoginPage from './../components/login-page'
+import fetcher from './../services/fetcher'
 
 class LoginPageContainer extends Component {
 
@@ -7,14 +8,37 @@ class LoginPageContainer extends Component {
     super(props)
     const { location } = props
     this.state = {
-      redirectError: location.state ? location.state.errorMessage : null
+      redirectError: location.state ? location.state.errorMessage : null,
+      emailOrPhone: '',
+      requestInProgress: false
     }
+  }
+
+  setEmailOrPhone (e) {
+    this.setState({emailOrPhone: e.target.value})
+  }
+
+  getAccessCode () {
+    // fetcher({
+    //   method: 'POST',
+    //   url: `${config.apiBaseUrl}/users`,
+    //   body: {},
+    //   beforeRequest: this.setState.call(this, {requestInProgress: true})
+    // }).then((res) => {
+    //   // let {json, status} = res
+    // }).catch((res) => {
+    //   // let {json, status} = res
+    // })
   }
 
   render () {
     return (
       <LoginPage
+        requestInProgress={this.state.requestInProgress}
         redirectError={this.state.redirectError}
+        setEmailOrPhone={this.setEmailOrPhone.bind(this)}
+        emailOrPhone={this.state.emailOrPhone}
+        getAccessCode={this.getAccessCode}
       />
     )
   }
