ba58cf6ba3d68b169a131905b415db616f1dfcb1
Eugene Lynch <eugene@enspiral.com>
2017-03-27 15:58:19 -0400

    remove facebook pixel shit

--- app/components/landing-page/index.js
+++ app/components/landing-page/index.js
@@ -28,10 +28,6 @@ class LandingPage extends Component {
         <HowItWorks content={content.howItWorks} />
 
         <LaunchingSoon content={content.launchingSoon} />
-
-        <script>
-          fbq('track', 'ViewContent')
-        </script>
       </div>
     )
   }
