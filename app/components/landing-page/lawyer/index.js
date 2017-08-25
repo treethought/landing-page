449f22bee50de021504b961260faaa4b6270f9bd
Eugene Lynch <eugene@enspiral.com>
2017-08-24 20:18:23 -0400

    tablet styling for everything except stories panel

--- app/components/landing-page/lawyer/index.js
+++ app/components/landing-page/lawyer/index.js
@@ -6,9 +6,7 @@ const Lawyer = () =>
       <div className='h2'>Talk to a lawyer earlier and not 15 minutes before arraignments</div>
       <p className='p'>Call 1-833-3-GOODCALL if you or a loved one get arrested to talk to a free lawyer can tell you what to expect and start working on the case</p>
     </div>
-    <div className='icon-container'>
-      <div className='icon' />
-    </div>
+    <img className='icon' src='http://www.arenakettering.co.uk/wp-content/uploads/grey-square.png' />
   </div>
 
 export default Lawyer
--- app/components/landing-page/loved-ones/index.js
+++ app/components/landing-page/loved-ones/index.js
@@ -6,9 +6,7 @@ const LovedOnes = () =>
       <div className='h2'>Make sure you and your loved ones can reach each other at the time of arrest</div>
       <p className='p'>Save an emergency contact for Good Call to alert in case of an arrest or be that contact for someone you want to be there for </p>
     </div>
-    <div className='icon-container'>
-      <div className='icon' />
-    </div>
+    <img className='icon' src='http://www.arenakettering.co.uk/wp-content/uploads/grey-square.png' />
   </div>
 
 export default LovedOnes
