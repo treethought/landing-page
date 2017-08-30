da39b53d26f529ba5eefbd0464911ccdad6f66fa
Eugene Lynch <eugene@enspiral.com>
2017-08-30 18:06:02 -0400

    IE config determines dev apiBaseUrl

--- app/config/index.js
+++ app/config/index.js
@@ -1,9 +1,10 @@
 const env = process.env.NODE_ENV || 'development'
 const isStaging = process.env.IS_STAGING
+const useIE = process.env.USE_IE
 
 const environmentConfigs = {
   development: {
-    apiBaseUrl: 'http://localhost:3000'
+    apiBaseUrl: useIE ? 'http://10.0.2.2:3000' : 'http://localhost:3000'
   },
   production: {
     apiBaseUrl: isStaging ? 'https://good-call-nyc-staging-api.herokuapp.com' : 'https://good-call-nyc-api.herokuapp.com'
