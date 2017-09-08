336fcecef2c991436a94a01a3e532ce31821c114
Eugene Lynch <eugene@enspiral.com>
2017-09-08 19:41:10 -0400

    fix footer overflow issue

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -23,65 +23,70 @@ const Footer = ({ content, subscribedToMailingList, subscribeToMailingList, erro
     { href: 'https://www.instagram.com/goodcallnyc', src: '/assets/imgs/instagram_icon.svg' }
   ]
 
+  const Meta = () =>
+    <div className='meta'>
+      <img src='/assets/imgs/logo-dark.png' className='logo' />
+      <a href='mailto:hello@goodcall.nyc' className='footer-link green-on-hover'>hello@goodcall.nyc</a>
+      <address>
+        <a
+          href='https://www.google.com/maps?q=150+Court+St.+2nd+Floor,+Brooklyn+11201'
+          className='footer-link green-on-hover'
+        >
+          {content.address}
+        </a>
+      </address>
+    </div>
+
+  const SiteNav = () =>
+    <div className='site-nav'>
+      <div className='footer-link links-header'>{content.siteNavHeader}</div>
+      {navLinks.map(({ to, label, onClick }) => (
+        <Link to={to} key={label} onClick={onClick} className='footer-link green-on-hover'>
+          {content[label]}
+        </Link>
+      ))}
+    </div>
+
+  const FollowUs = () =>
+    <div className='follow-us'>
+      <div className='footer-link links-header'>{content.followHeader}</div>
+      <div className='social-links'>
+        {socialLinks.map(({ href, src }) => (
+          <a href={href} className='social-link' key={src}>
+            <img src={src} className='social-media-logo' />
+          </a>
+        ))}
+      </div>
+
+      {subscribedToMailingList ? (
+        <div>{content.subscriptionConfirm}</div>
+      ) : (
+        <form className='mailing-list-form'>
+          <div className='inputs'>
+            <input
+              className='mailing-list-input'
+              placeholder={content.emailAddress}
+              onChange={setEmail}
+            />
+            <div className='mailing-list-button' onClick={subscribeToMailingList}>
+              <img src='/assets/imgs/subscribe-arrow.svg' />
+            </div>
+          </div>
+          {error && <div className='error'>{error}</div>}
+        </form>
+      )}
+    </div>
+
   return (
     <footer className='footer'>
       <div className='content'>
         <div className='main'>
-          <div className='meta'>
-            <img src='/assets/imgs/logo-dark.png' className='logo' />
-            <a href='mailto:hello@goodcall.nyc' className='footer-link green-on-hover'>hello@goodcall.nyc</a>
-            <address>
-              <a
-                href='https://www.google.com/maps?q=150+Court+St.+2nd+Floor,+Brooklyn+11201'
-                className='footer-link green-on-hover'
-              >
-                {content.address}
-              </a>
-            </address>
-          </div>
-
-          <div className='site-nav'>
-            <div className='footer-link links-header'>{content.siteNavHeader}</div>
-            {navLinks.map(({ to, label, onClick }) => (
-              <Link to={to} key={label} onClick={onClick} className='footer-link green-on-hover'>
-                {content[label]}
-              </Link>
-            ))}
-          </div>
-
-          <div className='follow-us'>
-            <div className='footer-link links-header'>{content.followHeader}</div>
-            <div className='social-links'>
-              {socialLinks.map(({ href, src }) => (
-                <a href={href} className='social-link' key={src}>
-                  <img src={src} className='social-media-logo' />
-                </a>
-              ))}
-            </div>
-
-            {subscribedToMailingList ? (
-              <div>{content.subscriptionConfirm}</div>
-            ) : (
-              <form className='mailing-list-form'>
-                <div className='inputs'>
-                  <input
-                    className='mailing-list-input'
-                    placeholder={content.emailAddress}
-                    onChange={setEmail}
-                  />
-                  <div className='mailing-list-button' onClick={subscribeToMailingList}>
-                    <img src='/assets/imgs/subscribe-arrow.svg' />
-                  </div>
-                </div>
-                {error && <div className='error'>{error}</div>}
-              </form>
-            )}
-          </div>
+          <Meta />
+          <SiteNav />
+          <FollowUs />
         </div>
 
-        <div className='non-profit'>
-          {content.nonProfit}
-        </div>
+        <div className='non-profit'>{content.nonProfit}</div>
       </div>
     </footer>
   )
