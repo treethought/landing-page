1cd6042dd395609c177ac5a24e4ed4e6a29abb86
Eugene Lynch <eugene@enspiral.com>
2018-01-05 14:03:31 -0500

    add fastco logo to landing page

--- app/components/landing-page/press/index.js
+++ app/components/landing-page/press/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const images = [
-  { file: 'univision.png', href: 'http://www.univision.com/nueva-york/wxtv/noticias/arrestos/una-campana-orienta-a-familias-de-el-bronx-sobre-como-pedir-auxilio-si-detienen-a-su-hijo' },
+  { file: 'univision.png', href: 'http://www.univision.com/nueva-york/wxtv/noticias/arrestos/una-campana-orienta-a-familias-de-el-bronx-sobre-como-pedir-auxilio-si-detienen-a-su-hijo', width: '42px' },
   { file: 'pns.png', href: 'http://www.publicnewsservice.org/2017-07-26/criminal-justice/hotline-brings-help-to-nyers-after-arrest/a58695-1' },
   { file: 'nyt.svg', href: 'https://www.nytimes.com/2017/07/23/nyregion/bronx-hotline-helps-people-make-the-right-call-after-an-arrest.html' },
-  { file: 'bronxnews12.png', href: 'http://bronx.news12.com/story/35713764/new-hotline-in-the-bronx-offers-legal-advice-for-criminal-offenders' },
+  { file: 'fast-company-logo.svg', href: 'https://www.fastcompany.com/40513626/these-poverty-fighting-startups-are-slaying-silicon-valleys-sacred-cows' },
+  { file: 'bronxnews12.png', href: 'http://bronx.news12.com/story/35713764/new-hotline-in-the-bronx-offers-legal-advice-for-criminal-offenders', width: '130px' },
   { file: 'bronxnet.jpg', href: 'http://www.bronxnet.org/tv/open/360-open-featured-interviews/9593-good-call-project-open-wednesday-june-21st-20172017-06-21-23-46-43' }
 ]
 
@@ -13,7 +14,7 @@ const Press = () =>
     <div className='images'>
       {images.map((img, i) => (
         <a href={img.href} key={i}>
-          <img src={`/assets/imgs/${img.file}`} className='image' data-fit='contain' />
+          <img src={`/assets/imgs/${img.file}`} className='image' data-fit='contain' style={{ width: img.width }} />
         </a>
       ))}
     </div>
